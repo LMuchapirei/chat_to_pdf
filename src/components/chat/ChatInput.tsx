bad4fb73ec24b465c6656e8e35fcec078a5e15bb
LMuchapirei <h170407h@hit.ac.zw>
2023-11-20 08:56:05 +0200

    finish setting up the chat functionality

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,5 +1,6 @@
+import { Send } from "lucide-react"
+import { Button } from "../ui/button"
 import { Textarea } from "../ui/textarea"
-
 interface ChatInputProps{
     isDisabled: boolean
 }
@@ -11,7 +12,17 @@ const ChatInput =({isDisabled}:ChatInputProps)=>{
                 <div className="relative flex h-full flex-1 items-stretch md:flex-col">
                     <div className="relative flex flex-col w-full flex-grow p-4">
                         <div className="relative">
-                            <Textarea placeholder="Enter your question...." rows={1} maxRows={4} />
+                            <Textarea 
+                             rows={1} 
+                             maxRows={4} 
+                             autoFocus
+                             placeholder="Enter your question...." 
+                             className
+                             ="resize-no pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+                             />
+                             <Button className="absolute bottom-1.5 right-[8px]" aria-label="send message">
+                                <Send className="h-4 w-4"/>
+                            </Button>
                         </div>
                     </div>
                 </div>
